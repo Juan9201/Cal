bbe6b176a4c80ada25d8e804e3591c1f51ba6ddc
Juan Garcia <tech.juan.garcia@gmail.com>
2025-06-10 18:04:47 -0500

    Placeholders are set to avoid any characters, numbers only

--- js/logical.js
+++ js/logical.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const input = document.querySelector(".thcount");
+  const inputs = document.querySelectorAll(".number");
 
-  input.addEventListener("input", function () {
-    this.value = this.value.replace(/[^0-9]/g, "");
+  inputs.forEach(function(input) {
+    input.addEventListener("input", function () {
+      let sanitizedValue = this.value.replace(/[^0-9.]/g, "");
+      
+const parts = sanitizedValue.split('.');
+    if (parts.length > 2) {
+        sanitizedValue = parts[0] + '.' + parts.slice(1).join('');
+    }
+    this.value = sanitizedValue;
+    });
   });
-});
