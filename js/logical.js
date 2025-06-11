07530eee12900c0f1abb75202f297bdf4968d028
Juan Garcia <tech.juan.garcia@gmail.com>
2025-06-10 19:30:34 -0500

    Calculator has been created wrong, but at least shows incorrect results, before was impossible; no it is!

--- js/logical.js
+++ js/logical.js
@@ -1,13 +1,49 @@
-  const inputs = document.querySelectorAll(".number");
+document.addEventListener("DOMContentLoaded", function () {
+    const inputs = document.querySelectorAll(".number");
 
-  inputs.forEach(function(input) {
-    input.addEventListener("input", function () {
-      let sanitizedValue = this.value.replace(/[^0-9.]/g, "");
-      
-const parts = sanitizedValue.split('.');
-    if (parts.length > 2) {
-        sanitizedValue = parts[0] + '.' + parts.slice(1).join('');
+    function calculation() {
+        const inputHeadCount = document.getElementById("a");
+        const inputBookingPrice = document.getElementById("b");
+        const spanResultado = document.getElementById("r");
+
+        const a = parseFloat(inputHeadCount.value);
+        const b = parseFloat(inputBookingPrice.value);
+
+    if (isNaN(a) || isNaN(b)) {
+            spanResultado.textContent = "Ingresa números válidos.";
+            return;
+        }
+
+        const resultCal = (((b / 100) * 20) - b) * a;
+        spanResultado.textContent = resultCal.toFixed(2);
     }
-    this.value = sanitizedValue;
+
+    inputs.forEach(function(input) {
+        input.addEventListener("input", function () {
+            let sanitizedValue = this.value.replace(/[^0-9.]/g, "");
+
+            const parts = sanitizedValue.split('.');
+            if (parts.length > 2) {
+                sanitizedValue = parts[0] + '.' + parts.slice(1).join('');
+            }
+            this.value = sanitizedValue;
+
+            calculation(); 
+        });
+
+        input.addEventListener("paste", function(event) {
+            const pasteData = event.clipboardData.getData('text');
+            event.preventDefault();
+            let sanitizedPasteData = pasteData.replace(/[^0-9.]/g, "");
+            const parts = sanitizedPasteData.split('.');
+            if (parts.length > 2) {
+                sanitizedPasteData = parts[0] + '.' + parts.slice(1).join('');
+            }
+            this.value = sanitizedPasteData;
+
+            calculation();
+        });
     });
-  });
+
+    calculation();
+});
